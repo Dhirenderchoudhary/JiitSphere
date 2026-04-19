712f3747850ba528467f8f2304f9d0ad2da57756
Dhirender Choudhary <dhirenderchoudhary0001@gmail.com>
2026-04-20 00:37:45 +0530

    fix: update matcher to exclude study-access from middleware redirects

--- web/src/middleware.js
+++ web/src/middleware.js
@@ -82,5 +82,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt|study-access|sitemap.xml).*)']
 };
